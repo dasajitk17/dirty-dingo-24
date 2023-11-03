c6975fc44c7c4e1df7546ac6ef56264992b0a631
Ajit <32097969+dasajitk17@users.noreply.github.com>
2023-11-03 02:03:32 -0500

    Update qa.js

--- pages/api/qa.js
+++ pages/api/qa.js
@@ -1,12 +1,5 @@
-
+// pages/api/hello.js
 
 export default (req, res) => {
-  // Simulate some data (you can fetch data from a database or another source here)
-  const data = {
-    message: "Hello, API!",
-    timestamp: new Date().toISOString(),
-  };
-
-  // Set the response status code and send the JSON data
-  res.status(200).json(data);
+  res.status(200).json({ message: 'Hello from the API!' });
 };
